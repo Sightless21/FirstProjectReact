1797af6b87fe7586ff6f89735e525bb84a467533
Sightless21 <Firstsecondd45@gmail.com>
2024-08-10 10:08:13 +0700

    Practice4_TextInput[10/8/2024]

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,40 +1,53 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native'
 import { stylesPractice } from './styles/styles'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Login = () => {
+const Login = (): React.JSX.Element => {
+
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+  const validateEmail = (email : string): boolean => {
+    const reCheckEmail = /\S+@\S+\.\S+/
+    return reCheckEmail.test(email);
+  }
+
+  const handleSubmit= () => {
+
+    let  errormassage = "";
+    if(!name){
+      /* Alert.alert("Alert",`Please Enter Name`,[{text: 'OK'}]); */
+      errormassage += "Please Enter Name\n";
+    }if(!email){
+      /* Alert.alert("Alert",`Please Enter Email`,[{text: 'OK'}]); */
+      errormassage += "Please Enter Email\n";
+    }else if(!validateEmail(email)){
+      errormassage += "Invalid email Format\n";
+    }
+
+    if(!password){
+      /* Alert.alert("Alert",`Please Enter Password`,[{text: 'OK'}]); */
+      errormassage += "Please Enter Password\n";
+    }else if(password.length <= 6){
+      Alert.alert("Alert",`Password must be 6 Character\n`);
+    }
+    
+    if(errormassage){
+      Alert.alert("Error",errormassage.trim(),[{ text: "OK"}]);
+      return;
+    }
+    Alert.alert("Alert","Success",[{ text: "OK"}]);
+  }
+
   return (
-    <View style={styles.container}>
-        <TextInput 
-        style = {styles.input}
-        placeholder='Enter name'/>
-        <TextInput 
-        style = {styles.input}
-        placeholder='Enter Email'/>
-        <Button title='Submit'/>
-      </View>
+    <View style={stylesPractice.containerLogin}>
+        <TextInput style = {stylesPractice.inputLogin} placeholder='Enter Name' onChangeText={setName}></TextInput>
+        <TextInput style = {stylesPractice.inputLogin} placeholder='Enter Email' onChangeText={setEmail}></TextInput>
+        <TextInput style = {stylesPractice.inputLogin} placeholder='Enter Password' onChangeText={setPassword} secureTextEntry></TextInput>
+        <Button title='Submit' onPress={handleSubmit}/>
+    </View>
   )
 }
 
-export default Login
-
-const styles = StyleSheet.create({
-    container: {
-        justifyContent: "center",
-        padding: 20,
-        marginTop: 50,
-        backgroundColor: '#ffffff', // ต้งัค่าสีพ้ืนหลงัเป็นสีขาว
-        borderRadius: 10, // เพิ่มความมนให้กบัขอบคอนเทนเนอร์
-        elevation: 3, // เพิ่มเงาให้กบัคอนเทนเนอร์(เฉพาะ Android)
-        width: '95%', // ต้งัความกวา้งของ container ให้เต็มหน้าจอ
-        },
-        input: {
-        height: 45, // เพิ่มความสูงของ TextInput
-        borderColor: "gray", // เปลี่ยนสีขอบเป็นสีเทาอ่อน
-        borderWidth: 1,
-        borderRadius: 8, // เพิ่มความมนให้กบัขอบ TextInput
-        marginBottom: 15, // เพิ่มระยะห่างดา้นล่างระหวา่ ง TextInput
-        paddingHorizontal: 15, // เพิ่มระยะห่างภายใน TextInput
-        backgroundColor: "#f9f9f9", // ต้งัค่าสีพ้ืนหลงัของ TextInput
-        }
-});
+export default Login;
